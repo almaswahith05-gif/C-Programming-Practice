bdd6d4bcb1ce91f7ba729fbc096522341299c4f1
almaswahith05-gif <almaswahith05@gmail.com>
2026-07-21 20:01:34 +0530

    Create subtraction.c
    added subtraction program

---
+++ subtraction.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, sub;
+
+    printf("Enter two numbers: ");
+    scanf("%d%d", &a, &b);
+
+    sub = a - b;
+
+    printf("Subtraction = %d", sub);
+
+    return 0;
+}
